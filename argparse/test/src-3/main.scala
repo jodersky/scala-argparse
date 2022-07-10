4b096b7f9af158309744d60316c3ccee7a09f648
Jakob Odersky <jakob@odersky.com>
2022-07-10 18:37:32 +0200

    Add support for nested commands to macro-based parser generator

--- argparse/test/src-3/main.scala
+++ argparse/test/src-3/main.scala
@@ -3,27 +3,61 @@ class Foo
 
 // object api extends argparse.core.Api with argparse.core.MainArgsApi
 
-val entrypoints = argparse.default.initialize()
 
-@argparse.main()
-def foo(value: Int = 2, paths: List[os.Path] = Nil) = {
-  println(value)
-  println(paths)
+
+class yo(base: Int) {
+
+  @argparse.main()
+  def foo(value: Int = base * 3, paths: List[os.Path] = Nil) = {
+    println(value)
+    println(paths)
+  }
+
+  // @argparse.main()
+  // def bar() = {
+
+  // }
+
+  @argparse.main()
+  def baz(q: Int) = {
+    println(base * q)
+  }
+
 }
 
-@argparse.main()
-def bar() = {
-
-}
+val entrypoints = argparse.default.initialize[yo]
 
 def main(args: Array[String]) = {
+  val parser = argparse.default.ArgumentParser()
+
+  val base = parser.param(
+    "--base",
+    1
+  )
+
+  entrypoints.addToParser(parser, yo(base.value))
+
+  // entrypoints.entrypoints.foreach{ ep =>
+  //   parser.command(ep.name, a => ep.invoke(yo(base.value))(a))
+  // }
+  parser.parseOrExit(args)
+
+
   //println(entrypoints.te)
   //println(entrypoints.methods)
 
-  val parser = argparse.default.ArgumentParser()
 
-  parser.param("--fooBar", "")
-  parser.parseOrExit(args)
+
+  // parser.param("--fooBar", "")
+  // parser.parseOrExit(args)
+
+  // entrypoints.entrypoint.foreach{ ep =>
+  //   println(ep.name)
+  //   // ep.run(yo(3), args)
+  // }
+
+
+  //entrypoints.entrypoints.head.invoke(yo(2), args)
 
   //entrypoints.methods.head.run(args)
 }
